f3cd8ba7aadde5d5e4ea008df3ac6783e3acf12c
Arthur Vinicius <thuur.vss@gmail.com>
2023-12-14 09:39:45 -0300

    criação das configs

---
+++ app/core/config.py
@@ -0,0 +1,17 @@
+from pydantic import BaseSettings
+
+class Settings(BaseSettings):
+    # Configurações do banco de dados
+    DATABASE_URL: str
+
+    # Outras configurações gerais
+    APP_NAME: str
+    APP_VERSION: str
+
+    class Config:
+        # Este atributo indica se as variáveis de ambiente devem ser lidas
+        # caso as mesmas não sejam passadas explicitamente na criação da instância de Settings.
+        env_file = ".env"
+
+settings = Settings()
+
