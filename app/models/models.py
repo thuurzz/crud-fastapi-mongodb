968df6f4be14f86bf94fd82bc6440b2065e68167
Arthur Vinicius <thuur.vss@gmail.com>
2023-12-15 09:27:25 -0300

    wip: bug pydantic

--- app/models/models.py
+++ app/models/models.py
@@ -1,4 +1,6 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, GetJsonSchemaHandler
+from pydantic_core import CoreSchema
+from typing import Any, Dict
 from typing import Optional
 from bson import ObjectId
 from datetime import datetime
@@ -15,8 +17,13 @@ class PyObjectId(ObjectId):
         return ObjectId(v)
 
     @classmethod
-    def __modify_schema__(cls, field_schema):
-        field_schema.update(type="string")
+    def __get_pydantic_json_schema__(
+        cls, core_schema: CoreSchema, handler: GetJsonSchemaHandler
+    ) -> Dict[str, Any]:
+        json_schema = super().__get_pydantic_json_schema__(core_schema, handler)
+        json_schema = handler.resolve_ref_schema(json_schema)
+        json_schema.update(type="string")
+        return json_schema
 
 class PersonBase(BaseModel):
     name: str
@@ -36,7 +43,7 @@ class Person(PersonBase):
     class Config:
         arbitrary_types_allowed = True
         json_encoders = {ObjectId: str}
-        schema_extra = {
+        json_schema_extra = {
             "example": {
                 "name": "John Doe",
                 "email": "johndoe@example.com",
