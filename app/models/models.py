273887425aecf81deb00c96c071bd66e48703a51
Arthur Vinicius <thuur.vss@gmail.com>
2023-12-15 10:03:53 -0300

    fix: troca de _id por id

--- app/models/models.py
+++ app/models/models.py
@@ -1,29 +1,7 @@
-from pydantic import BaseModel, Field, GetJsonSchemaHandler
-from pydantic_core import CoreSchema
-from typing import Any, Dict
+from pydantic import BaseModel, Field
 from typing import Optional
-from bson import ObjectId
 from datetime import datetime
-
-class PyObjectId(ObjectId):
-    @classmethod
-    def __get_validators__(cls):
-        yield cls.validate
-
-    @classmethod
-    def validate(cls, v):
-        if not ObjectId.is_valid(v):
-            raise ValueError("Invalid object id")
-        return ObjectId(v)
-
-    @classmethod
-    def __get_pydantic_json_schema__(
-        cls, core_schema: CoreSchema, handler: GetJsonSchemaHandler
-    ) -> Dict[str, Any]:
-        json_schema = super().__get_pydantic_json_schema__(core_schema, handler)
-        json_schema = handler.resolve_ref_schema(json_schema)
-        json_schema.update(type="string")
-        return json_schema
+import uuid
 
 class PersonBase(BaseModel):
     name: str
@@ -37,16 +15,11 @@ class PersonUpdate(PersonBase):
     name: Optional[str] = None
 
 class Person(PersonBase):
-    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, alias="id")
     created_at: datetime = Field(default_factory=datetime.utcnow)
 
     class Config:
-        arbitrary_types_allowed = True
-        json_encoders = {ObjectId: str}
-        json_schema_extra = {
-            "example": {
-                "name": "John Doe",
-                "email": "johndoe@example.com",
-                "age": 30
-            }
+        json_encoders = {
+            uuid.UUID: lambda v: str(v),  # Convert UUID to string for JSON responses
+            datetime: lambda v: v.isoformat()  # Convert datetime to ISO format string
         }
--- app/services/person_service.py
+++ app/services/person_service.py
@@ -1,30 +1,36 @@
 from ..db.database import get_collection
-from bson import ObjectId
 from ..models.models import PersonCreate, Person
-
-def add_person(person_data: PersonCreate) -> Person:
-    collection = get_collection("people")
-    result = collection.insert_one(person_data.dict())
-    return Person(**person_data.dict(), id=result.inserted_id)
+from bson import Binary
+import uuid
 
 def get_all_people() -> list:
     collection = get_collection("people")
     people = collection.find()
     return [Person(**person) for person in people]
 
+def add_person(person_data: PersonCreate) -> Person:
+    collection = get_collection("people")
+    person_dict = person_data.model_dump()
+    person_dict['id'] = Binary(uuid.uuid4().bytes, 4)  # Convertendo UUID para BSON Binary
+    collection.insert_one(person_dict)
+    return Person(**person_dict)
+
 def find_person(person_id: str) -> Person:
     collection = get_collection("people")
-    person = collection.find_one({"_id": ObjectId(person_id)})
+    person = collection.find_one({"id": Binary(uuid.UUID(person_id).bytes, 4)})
     if person:
         return Person(**person)
     return None
 
 def update_person(person_id: str, person_data: PersonCreate) -> Person:
     collection = get_collection("people")
-    collection.update_one({"_id": ObjectId(person_id)}, {"$set": person_data.dict()})
+    collection.update_one(
+        {"id": Binary(uuid.UUID(person_id).bytes, 4)}, 
+        {"$set": person_data.model_dump(exclude={'id'})}
+    )
     return find_person(person_id)
 
 def delete_person(person_id: str) -> bool:
     collection = get_collection("people")
-    result = collection.delete_one({"_id": ObjectId(person_id)})
+    result = collection.delete_one({"id": Binary(uuid.UUID(person_id).bytes, 4)})
     return result.deleted_count > 0
